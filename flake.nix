f25ed32480158c0f436b6c35f18a730c3907786a
mrcjkb <12857160+mrcjkb@users.noreply.github.com>
2025-07-27 13:11:36 +0000

    GitHub Action automated Zen update

--- flake.nix
+++ flake.nix
@@ -8,9 +8,9 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.14.8b";
+      version = "1.14.9b";
       download.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-	    download.sha256 = "10a1skdfl569zmyiy8191gvcys6hj7nnzv2sq9y6laic77s9y8nd";
+	    download.sha256 = "1k27vl9b2y87skwy1mg955n74x504dvz7w3r2yxir6ib2srnslzi";
 
       pkgs = import nixpkgs {
         inherit system;
